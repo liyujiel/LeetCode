dda7497b6fbf8f91f50c2b7a73aca0c01fdceb5e
Alex Li <li1995717@gmail.com>
2017-06-22 15:42:39 -0400

    modify rotate3

--- rotateMatrix.py
+++ rotateMatrix.py
@@ -12,5 +12,6 @@ def rotate2(self,matrix):
 def rotate3(self,matrix):
     n = len(matrix)
     for i in range(n/2):
-        for j in range(n/2):
+        # n-n/2 I guess is for chop left
+        for j in range(n-n/2):
             matrix[i][j],matrix[~j][i],matrix[~i][~j],matrix[j][~i] = matrix[~j][i],matrix[~i][~j],matrix[j][~i],matrix[i][j]
