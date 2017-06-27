adfde832c0f4ffb077e3fa3ab4a60cb6929644f1
Alex Li <li1995717@gmail.com>
2017-06-27 15:56:16 -0400

    add prefixTree

---
+++ PrefixTree.py
@@ -0,0 +1,56 @@
+class TrieNode():
+    def __init__(self,key):
+        self.word = 0
+        self.key = key
+        self.children = {}
+
+class Trie(object):
+    
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.root = TrieNode(None)
+        
+        
+
+    def insert(self, word):
+        """
+        Inserts a word into the trie.
+        :type word: str
+        :rtype: void
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TrieNode(char)
+            node = node.children[char]
+        node.children += 1
+
+    def search(self, word):
+        """
+        Returns if the word is in the trie.
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.word
+
+        
+
+    def startsWith(self, prefix):
+        """
+        Returns if there is any word in the trie that starts with the given prefix.
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for pos in prefix:
+            if pos not in node.children:
+                return False
+            node = node.children[pos]
+        return True
