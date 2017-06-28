4541cb0694e1e60e4d7943a56779c42add327612
Alex Li <li1995717@gmail.com>
2017-06-28 17:13:32 -0400

    add LRU.py

---
+++ LRU.py
@@ -0,0 +1,89 @@
+from collections import OrderedDict
+class LRUCache(object):
+    
+    def __init__(self, capacity):
+        """
+        :type capacity: int
+        """
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key):
+        """
+        :type key: int
+        :rtype: int
+        """
+        res = -1
+        if key in self.cache:
+            res = self.cache.pop(key)
+            self.cache[key] = res
+        return res
+        
+
+    def put(self, key, value):
+        """
+        :type key: int
+        :type value: int
+        :rtype: void
+        """
+        if key in self.cache:
+            self.cache.pop(key)
+        else:
+            if self.capacity > 0:
+                self.capacity -= 1
+            else:
+                self.cache.popitem(last=False)
+        self.cache[key] = value
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
+
+class Node:
+    def __init__(self, k, v):
+        self.key = k
+        self.val = v
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.dic = dict()
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+    
+    def get(self, key):
+        if key in self.dic:
+            n = self.dic[key]
+            self._remove(n)
+            self._add(n)
+            return n.val
+        return -1
+    
+    def put(self, key, value):
+        if key in self.dic:
+            self._remove(self.dic[key])
+        n = Node(key, value)
+        self._add(n)
+        self.dic[key] = n
+        if len(self.dic) > self.capacity:
+            n = self.head.next
+            self._remove(n)
+            del self.dic[n.key]
+    
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+    
+    def _add(self, node):
+        p = self.tail.prev
+        p.next = node
+        self.tail.prev = node
+        node.prev = p
+        node.next = self.tail
