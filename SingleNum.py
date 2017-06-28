a9b1e714decce4168bb3db15cd5d27042441b13d
Alex Li <li1995717@gmail.com>
2017-06-28 15:08:30 -0400

    add dict and lamda solution

--- SingleNum.py
+++ SingleNum.py
@@ -10,3 +10,15 @@ from functools import reduce
 
 def singleNum2(self,nums):
     return reduce(operator.xor, nums)
+
+def singleNum3(self,nums):
+    return reduce(lambda x, y:x^y, nums)
+
+
+def singleNum4(self,nums):
+    dic = {}
+    for num in nums:
+        dic[num] = dic.get(num,0)+1
+    for key,val in dic:
+        if val == 1:
+            return key
