0236e9223f1125522485e796f24cbb17c13a8130
Alex Li <li1995717@gmail.com>
2017-06-29 13:28:36 -0400

    add FDFS and WS

---
+++ WordSearchII.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+        """
+        trie = {}
+        for w in words:
+            t = trie
+            for c in w:
+                if c not in t:
+                    t[c] = {}
+                t = t[c]
+            t['#'] = "#"
