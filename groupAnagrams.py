762848d6e863ddcfda3638442f6a8a5dd0eeefc3
Alex Li <li1995717@gmail.com>
2017-07-18 12:57:50 -0400

    add solution2

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -5,3 +5,10 @@ def groupAnagrams(strs):
     for s in strs:
         groups[tuple(sorted(s))].append(s)
     return sorted(groups.values())
+
+
+import itertools
+
+def groupAnagrams2(strs):
+    groups = itertools.groupby(sorted(strs,key=sorted),sorted)
+    return [sorted(members) for _, members in groups]
